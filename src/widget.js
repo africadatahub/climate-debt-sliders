8c61189007b93917b9f8045069d12cc59e5c39f9
Di M <meerkdir@gmail.com>
2023-09-07 10:42:14 +0200

    Movable emissions

--- src/widget.js
+++ src/widget.js
@@ -48,14 +48,14 @@ export function Widget() {
             energy_demand_2030: 7773*1.65,
             energy_demand_2050: 7773*2.1
         },
-        {
-            consumption_level: 'EU AVG',
-            population_estimate_2030: 1.65,
-            population_estimate_2050: 2.1,
-            consumption_per_capita_kwh: 9000,
-            energy_demand_2030: 9000*1.65,
-            energy_demand_2050: 9000*2.1
-        },
+        // {
+        //     consumption_level: 'EU AVG',
+        //     population_estimate_2030: 1.65,
+        //     population_estimate_2050: 2.1,
+        //     consumption_per_capita_kwh: 9000,
+        //     energy_demand_2030: 8000*1.65,
+        //     energy_demand_2050: 8000*2.1
+        // },
         {
             consumption_level: 'US AVG',
             population_estimate_2030: 1.65,
@@ -65,9 +65,9 @@ export function Widget() {
             energy_demand_2050: 12744*2.1
         }
     ]);
-    const [consumptionScale, setConsumptionScale] = React.useState({});
+    const [consumptionScale, setConsumptionScale] = React.useState();
     const [consumptionPerCapita, setConsumptionPerCapita] = React.useState(560);
-    const [consumptionPerCapitaPercent, setConsumptionPerCapitaPercent] = React.useState(4.39);
+    const [consumptionPerCapitaPercent, setConsumptionPerCapitaPercent] = React.useState(560/12744*100);
 
     const [energyMixScale, setEnergyMixScale] = React.useState({
         0: '0',
@@ -78,17 +78,18 @@ export function Widget() {
         100: '100'
     });
     const [energyMix, setEnergyMix] = React.useState(20.6);
-    const [emissions, setEmissions] = React.useState(560*20.6);
+    const [emissions, setEmissions] = React.useState(((560*2.1)/100)*(100-20.6) * (1106/1000000));
     const [emissionsScale, setEmissionsScale] = React.useState({
         0: '0',
         3.489: 'Current'
     });
-    const [emissionsPercent, setEmissionsPercent] = React.useState(0);
+    const [emissionsPercent, setEmissionsPercent] = React.useState(3.489);
 
     const [fossilFuelMakeup, setFossilFuelMakeup] = React.useState(1106);
 
-    const [lockedSlider, setLockedSlider] = React.useState('emissions');
+    const [lockedSlider, setLockedSlider] = React.useState('');
 
+    const [lastSlider, setLastSlider] = React.useState('');
 
     const [annotations, setAnnotations] = React.useState(
         {
@@ -184,24 +185,27 @@ export function Widget() {
         let cpc = Math.round(consumptionData[consumptionData.length - 1].consumption_per_capita_kwh * (value/100));
         setConsumptionPerCapita(cpc);
         setConsumptionPerCapitaPercent(value);
+        setLastSlider('consumption');
     }
 
     let changeEnergyMix = (value) => {
         setEnergyMix(value);
+        setLastSlider('energyMix');
     }
 
     let changeEmissions = (value) => {
-        setEmissions(value);
-         let consumption = emissions / ((2.1/100)*(100-energyMix) * (fossilFuelMakeup/1000000));
-        setConsumptionPerCapita(consumption);
+        setLastSlider('emissions');
+        setEmissionsPercent(value);
     }
 
     let changeFossilFuelMakeup = (value) => {
         setFossilFuelMakeup(value);
+        setLastSlider('fossilFuelMakeup');
     }
 
-    useEffect(() => {
-        let co2 = ((consumptionPerCapita*2.1)/100)*(100-energyMix) * (fossilFuelMakeup/1000000);
+    let updateEmissions = () => {
+        let co2 = ((consumptionPerCapita*2.1)/100)*(100-energyMix)*(fossilFuelMakeup/1000000);
+
         setEmissions(co2);
 
         let maxEmissions = 12744*2.1 * (1106/1000000);
@@ -209,8 +213,41 @@ export function Widget() {
         let co2Percent = (co2/maxEmissions)*100;
 
         setEmissionsPercent(co2Percent);
+    }
 
+    useEffect(() => {
+        if(lastSlider != 'emissions') {
+
+            if(lockedSlider == 'emissions') {
+                if(lastSlider == 'consumption') {
+                    let energyMix = 100 - ((100*emissions*1000000)/(consumptionPerCapita/2.1/fossilFuelMakeup));
+                    setEnergyMix(energyMix);
+                } else {
+                    let consumption = (emissions*1000000*(100-energyMix))/(100*2.1*fossilFuelMakeup);
+                    setConsumptionPerCapita(consumption);
+                    setConsumptionPerCapitaPercent(consumption/consumptionData[consumptionData.length - 1].consumption_per_capita_kwh*100);
+                }
+            } else {
+                updateEmissions();
+            }
+        }
     }, [consumptionPerCapita, energyMix, fossilFuelMakeup]);
+
+    useEffect(() => {
+        if(lastSlider == 'emissions') {
+            let maxEmissions = 12744*2.1 * (1106/1000000);
+            let emissions = (maxEmissions/100)*emissionsPercent;
+
+            let consumption = (100*emissions)/(2.1*(100-energyMix)*(fossilFuelMakeup/1000000));
+
+            setConsumptionPerCapita(consumption);
+            setConsumptionPerCapitaPercent(consumption/consumptionData[consumptionData.length - 1].consumption_per_capita_kwh*100);
+            
+            setEmissions(emissions);
+        }
+    }, [emissionsPercent]);
+
+    
 
    
 
@@ -237,7 +274,7 @@ export function Widget() {
                     <h1>Projected GHG emissions<div className="subtitle">based on energy consumption and sources in Africa</div></h1>
                 </header>
 
-                <div class="sliders-wrapper">
+                <div className="sliders-wrapper">
 
                     {/***** CONSUMPTION *****/}
 
@@ -300,7 +337,7 @@ export function Widget() {
                                             annotations.consumption.map((item, index) => {
                                                 if (consumptionPerCapitaPercent >= item.value_start && consumptionPerCapitaPercent <= item.value_end) {
                                                     return (
-                                                        <Row className="annotation">
+                                                        <Row className="annotation" key={index}>
                                                             <Col xs={12} md="auto">
                                                                 <h2 className="text-consumption">{item.title}</h2>
                                                             </Col>
@@ -369,17 +406,13 @@ export function Widget() {
                             </Row>
                         </div>
 
-                        {/* <div className="tooltip-row d-none d-md-block">
-                            <div class="slider-tooltip" style={{left: tooltips.energyMix.position}}>
-                                {tooltips.energyMix.text}
-                            </div>
-                        </div> */}
+                       
                         
                         <div className="slider-row">
                             <Row>
                                 <Col>
                                     <div className="slider-container">
-                                        <div class="slider-tooltip" style={{left: '45.6%'}}>
+                                        <div className="slider-tooltip" style={{left: '45.6%'}}>
                                             <svg viewBox="0 0 100 125"><path d="M17.7,19.8c2.7-0.8,5.7-0.8,8.6-0.4c3,0.4,5.9,1.3,8.7,2.4c5.3,2.1,10.2,5,14.6,8.6  c9.4,7.8,16.2,18.2,20.4,29.6c1.6,4.4,2.8,8.8,3.8,13.4C73,72.3,72,71.1,71,69.9c-3.1-3.7-6.5-7.3-10.1-10.6  c-0.7-0.6-1.8-0.5-2.6-0.2c-0.4,0.1-1.8,0.9-1.1,1.5c3.4,3.2,6.7,6.6,9.7,10.2c1.5,1.8,2.9,3.6,4.3,5.4c0.7,0.9,1.4,1.8,2,2.8  c0.4,0.5,0.7,1,1.1,1.5c0.2,0.4,0.7,0.8,0.5,1.2c-0.1,0.4,0.1,0.6,0.4,0.8c0.4,0.3,1.3,0.4,1.6,0.4c0.9-0.1,1.9-0.4,2.3-1.2  c3.7-7.2,5.7-15.2,5.8-23.3c0-0.3-0.5-0.5-0.7-0.6c-0.5-0.1-1-0.1-1.5,0C82.1,58,81,58.4,81,59.2c-0.1,5.3-1,10.5-2.7,15.5  c-2.1-11.4-6.1-22.5-12.6-32.1c-6.6-9.7-15.9-17.9-26.8-22.4c-7.2-2.9-15-4.3-22.6-2.1c-0.5,0.2-1.7,0.8-1.1,1.5  C15.7,20.4,17.1,20,17.7,19.8z"/></svg><div>{tooltips.energyMix.text}</div>
                                         </div>
                                         <Slider
@@ -412,7 +445,7 @@ export function Widget() {
                                             annotations.energy_mix.map((item, index) => {
                                                 if (energyMix >= item.value_start && energyMix <= item.value_end) {
                                                     return (
-                                                        <Row className="annotation">
+                                                        <Row className="annotation" key={index}>
                                                             <Col xs={12} md="auto">
                                                                 <h2 className="text-energymix">{item.title}</h2>
                                                             </Col>
@@ -445,7 +478,7 @@ export function Widget() {
                                 </Col>
                                 <Col className="d-none d-md-block">
                                     <div className="annotation-small">
-                                        Measured as gigatons (GT) of C0<sub>2</sub> equivalent emissions per year
+                                        Measured as gigatons (GT) of CO<sub>2</sub> equivalent emissions per year
                                     </div>
                                 </Col>
                                 <Col className="d-md-none">
@@ -461,11 +494,7 @@ export function Widget() {
                             </Row>
                         </div>
 
-                        {/* <div className="tooltip-row d-none d-md-block">
-                            <div class="slider-tooltip" style={{left: tooltips.emissions.position}}>
-                                {tooltips.emissions.text}
-                            </div>
-                        </div> */}
+                       
 
                         <div className="slider-row">
                             <Row>
@@ -492,11 +521,7 @@ export function Widget() {
                             </Row>
                         </div>
 
-                        {/* <div className="tooltip-row d-md-none">
-                            <div class="slider-tooltip" style={{left: tooltips.emissions.position}}>
-                                {tooltips.emissions.text}
-                            </div>
-                        </div> */}
+                       
 
                         <div className="annotation-row">
                             <Row>
